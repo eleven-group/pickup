8690e420db6b6ee2f8eece7a99f9e8c709ee7c5c
VARVEROPOULOS Valentin <v.varveropou.ext_azu@sodexo.com>
2020-03-30 15:40:58 +0200

    upgrade symfony version from 4.2 to 4.4

--- api/src/Repository/PayslipRepository.php
+++ api/src/Repository/PayslipRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Payslip;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Payslip|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class PayslipRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Payslip::class);
     }
