e8f20f6bd7a5b3c6a6050e6329c0fc3f0c61a225
Théo Senoussaoui <theosen95@gmail.com>
2020-04-02 22:59:22 +0200

    style: complete rework of the homepage

--- front/src/components/Map/index.vue
+++ front/src/components/Map/index.vue
@@ -1,28 +1,47 @@
 <template>
-  <div>
-    <l-map class="map" :zoom="zoom" :center="center" @update:center="centerUpdate" @update:zoom="zoomUpdate">
-      <l-tile-layer :url="url" :attribution="attribution" />
-      <l-marker v-for="(item, index) in markers" :key="'marker-' + index" :latLng="item.location" :icon="getIcon()">
-        <Popup :item="item"/>
-      </l-marker>
-    </l-map>
-    <div v-for="layer in layers" :key="layer.id">
-      <label class="form-check-label">
-        <input
-          class="form-check-input"
-          type="checkbox"
-          v-model="layer.active"
-          @change="layerChanged(layer.id, layer.active)"
-        />
-        {{ layer.name }}
-      </label>
-    </div>
+  <div class="map-container">
+    <el-row>
+      <el-col :span="24" :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
+        <el-card>
+          <div v-for="layer in layers" :key="layer.id">
+            <el-checkbox
+              v-model="layer.active"
+              @change="layerChanged(layer.id, layer.active)"
+            >{{ layer.name }}</el-checkbox>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+    <el-row class="map">
+      <l-map
+        class="leaflet"
+        :zoom="zoom"
+        :center="center"
+        @update:center="centerUpdate"
+        @update:zoom="zoomUpdate"
+      >
+        <l-tile-layer :url="url" :attribution="attribution" />
+        <l-control class="example-custom-control">
+          <el-input placeholder="Cherchez votre adresse" v-model="input">
+            <i slot="prefix" class="el-input__icon el-icon-search"></i>
+          </el-input>
+        </l-control>
+        <l-marker
+          v-for="(item, index) in markers"
+          :key="'marker-' + index"
+          :latLng="item.location"
+          :icon="getIcon()"
+        >
+          <Popup :item="item" />
+        </l-marker>
+      </l-map>
+    </el-row>
   </div>
 </template>
 
 <script>
 import L, { latLng } from 'leaflet';
-import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+import { LMap, LTileLayer, LMarker, LControl } from 'vue2-leaflet';
 import { mapState, mapActions } from 'vuex';
 
 import Popup from '@/components/PopUp/';
@@ -33,10 +52,12 @@ export default {
     LMap,
     LTileLayer,
     LMarker,
+    LControl,
     Popup
   },
   data () {
     return {
+      input: '',
       map: null,
       center: latLng(48.856274, 2.354124),
       currentCenter: latLng(48.856274, 2.354124),
@@ -49,8 +70,7 @@ export default {
           features: []
         }
       ],
-      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      url: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
       zoom: 16,
       currentZoom: 11.5,
       markers: []
@@ -79,17 +99,16 @@ export default {
     }
   },
   methods: {
-    ...mapActions('shops', [
-      'fetchShops'
-    ]),
+    ...mapActions('shops', ['fetchShops']),
     getIcon () {
       return L.divIcon({
-        className: 'my-custom-pin',
-        html: `<svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <circle cx="32" cy="32" r="12" fill="#0062FF"/>
-                  <circle cx="32" cy="32" r="12" fill="#0062FF"/>
-                  <circle cx="32" cy="32" r="32" fill="#0062FF" fill-opacity="0.18"/>
-              </svg>`
+        className: 'marker',
+        html: `<svg width="52" height="42" viewBox="0 0 52 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="21" cy="21" r="7.875" fill="#0062FF"/>
+                <circle cx="21" cy="21" r="7.875" fill="#0062FF"/>
+                <circle cx="21" cy="21" r="21" fill="#0062FF" fill-opacity="0.18"/>
+                </svg>
+              `
       });
     },
     initMap () {
@@ -99,22 +118,25 @@ export default {
     initLayers () {
       const { shops } = this.$store.state.shops;
       shops.forEach((shop, index) => {
+        const geoCode = shop.geocode.split(',');
         this.layers.forEach(layer => {
-          layer.features.push(
-            {
-              id: index,
-              name: shop.name,
-              description: shop.description,
-              type: 'marker',
-              coords: latLng(shop.latitude, shop.longitude)
-            }
-          );
+          layer.features.push({
+            id: index,
+            name: shop.name,
+            description: shop.description,
+            type: 'marker',
+            coords: latLng(geoCode[0], geoCode[1])
+          });
         });
       });
       this.layers.forEach(layer => {
-        const markerFeatures = layer.features.filter(feature => feature.type === 'marker');
-        markerFeatures.forEach((feature) => {
-          feature.leafletObject = L.marker(feature.coords).bindPopup(feature.name);
+        const markerFeatures = layer.features.filter(
+          feature => feature.type === 'marker'
+        );
+        markerFeatures.forEach(feature => {
+          feature.leafletObject = L.marker(feature.coords).bindPopup(
+            feature.name
+          );
         });
       });
     },
@@ -147,14 +169,31 @@ export default {
 };
 </script>
 
-<style scoped>
-  .map {
-    height: 700px;
-    width: 1000px;
-    border: 1px solid black;
-  }
+<style lang="scss" scoped>
+.map-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: stretch;
+  height: 100%;
+}
 
-  .my-custom-pin{
-    background-color: transparent;
+.map {
+  width: 100%;
+  flex-grow: 2;
+  > .leaflet {
+    border-radius: 20px;
+    border: 1px solid #e2e2ea;
   }
+}
+
+.el-checkbox {
+  @media (max-width: 992px) {
+    margin: 12px 0;
+  }
+}
+
+.marker {
+  background-color: transparent;
+}
 </style>
