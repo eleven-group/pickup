a1880827601e1ef1eca64a46a3a197a00e786547
VARVEROPOULOS Valentin <v.varveropou.ext_azu@sodexo.com>
2020-04-03 17:03:11 +0200

    Add mail for bookings

--- api/src/EventListener/ResourceCreateEventListener.php
+++ api/src/EventListener/ResourceCreateEventListener.php
@@ -3,10 +3,11 @@
 namespace App\EventListener;
 
 use App\Entity\User;
+use App\Entity\Booking;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
 use App\Service\Mailer;
 
-class UserCreateSuccessListener
+class ResourceCreateEventListener
 {
 
     private $mailer;
@@ -19,18 +20,25 @@ class UserCreateSuccessListener
     public function postPersist(LifecycleEventArgs $args)
     {
         $entity = $args->getObject();
-
-        if (!$entity instanceof User) {
+        if (!$entity instanceof User && !$entity instanceof Booking) {
             return;
         }
-
+        
         if ('cli' != php_sapi_name())
         {
-            $this->mailer->sendMail($entity->getEmail(), 5, array(
+            $templateId = $entity instanceof User ? 5 : 2;
+            $params = $entity instanceof User ? 
+            array(
                 'token' => $entity->getToken(),
                 'firstname' => $entity->getFirstname(),
                 'lastname' => $entity->getLastname(),
-            ));
+            ) : 
+            array(
+                'shipping_date' => $entity->getDate(),
+                'price' => $entity->getTotal(),
+            );
+            
+            $this->mailer->sendMail($entity->getEmail(), $templateId, $params);
         }
 
     }
---
+++ api/src/EventListener/ResourceUpdateEventListener.php
@@ -0,0 +1,40 @@
+<?php 
+
+namespace App\EventListener;
+
+use App\Entity\Booking;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+use App\Service\Mailer;
+
+class ResourceUpdateEventListener
+{
+
+    private $mailer;
+
+    public function __construct(Mailer $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
+    public function postUpdate(LifecycleEventArgs $args)
+    {
+        $entity = $args->getObject();
+
+        if (!$entity instanceof Booking) {
+            return;
+        }
+
+        if ('cli' != php_sapi_name())
+        {
+            $templateId = $entity->getStatus() === 'accepted' ? 3 : 4;
+            $params = $entity->getStatus() === 'accepted' ? 
+            array(
+                'shipping_date' => $entity->getDate(),
+                'price' => $entity->getTotal(),
+            ) : array();
+            
+            $this->mailer->sendMail($entity->getEmail(), $templateId, $params);
+        }
+
+    }
+}
