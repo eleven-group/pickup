49eaec5e643ac0f6cb868ec9187ef9bcccd526be
Louis Harang <narah@pm.me>
2020-04-03 18:57:17 +0200

    feat: add constats

--- front/src/constants/index.js
+++ front/src/constants/index.js
@@ -1,17 +1,16 @@
 export const statusBooking = [
-{ id: "pending", name:"En attente de confirmation" },
-{ id: "accepted", name:"En cours" },
-{ id: "done", name:"Terminée" },
-{ id: "canceled", name:"Annulée" },
+  { id: 'pending', name: 'En attente de confirmation' },
+  { id: 'accepted', name: 'En cours' },
+  { id: 'done', name: 'Terminée' },
+  { id: 'canceled', name: 'Annulée' }
 ];
 
-
 export const categoriesName = {
-  pizza: "Pizza",
-  burger: "Burger",
-  producer: "Producteur",
-  baker: "Boulanger",
-  butcher: "Boucher",
-  pastry: "Pâtissier",
-  other: "Autres",
+  pizza: 'Pizza',
+  burger: 'Burger',
+  producer: 'Producteur',
+  baker: 'Boulanger',
+  butcher: 'Boucher',
+  pastry: 'Pâtissier',
+  other: 'Autres'
 };
